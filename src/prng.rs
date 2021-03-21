0b9c9b2fd9221d43553f5fa561ecd7ded96321e5
cympfh <cympfh@gmail.com>
2021-03-21 15:59:11 +0900

    bugfix

--- src/prng.rs
+++ src/prng.rs
@@ -25,10 +25,42 @@ impl PRNG {
 
     pub fn exponential(&mut self, lambda: f64) -> f64 {
         let x = self.uniform(0.0, 1.0);
-        -x / lambda
+        -x.ln() / lambda
     }
 
     pub fn setseed(&mut self, state: u64) {
         self.rng = ChaCha8Rng::seed_from_u64(state);
+    }
+}
+
+#[cfg(test)]
+mod test_parser {
+    use crate::prng::*;
+    #[test]
+    fn seeding() {
+        let mut rng = PRNG::new();
+        {
+            rng.setseed(42);
+            let x = rng.uniform(10.0, 20.0);
+            rng.setseed(42);
+            let y = rng.uniform(10.0, 20.0);
+            assert_eq!(x, y);
+        }
+        {
+            rng.setseed(42);
+            let x = rng.gaussian(2.0, 1.0);
+            rng.setseed(42);
+            let y = rng.gaussian(2.0, 1.0);
+            assert_eq!(x, y);
+        }
+    }
+
+    #[test]
+    fn test_exponential() {
+        let mut rng = PRNG::new();
+        for _ in 0..10 {
+            assert!(rng.exponential(0.5) > 0.0);
+            assert!(rng.exponential(1.0) > 0.0);
+        }
     }
 }
